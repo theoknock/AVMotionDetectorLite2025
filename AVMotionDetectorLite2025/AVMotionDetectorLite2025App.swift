3792adf39d02622618180c9ab408eb1c68e4e28b
James Alan Bush <theoknock@gmail.com>
2025-04-22 22:32:16 -0400

    Initial Commit

---
+++ AVMotionDetectorLite2025/AVMotionDetectorLite2025App.swift
@@ -0,0 +1,17 @@
+//
+//  AVMotionDetectorLite2025App.swift
+//  AVMotionDetectorLite2025
+//
+//  Created by Xcode Developer on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AVMotionDetectorLite2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
